d3bf552eb79f79ec02c8ccc73ffae56680875efc
Behrad <soltanibehrad@gmail.com>
2026-02-25 18:02:15 +0330

    Optimised the gen_grid function by turning x & y into a tuple

--- grid_system.py
+++ grid_system.py
@@ -1,14 +1,14 @@
 import random
 def generate_grid(size):
-    grid_attribute = ["tree", "plain", "food"]
+    terrain_types = ["tree", "plain", "food"]
     grid = {}
     counts = {"tree": 0, "plain": 0, "food": 0}
-    for x in range(size + 1):
-        for y in range(size + 1):
-            point = (x, y)
-            tile = random.choice(grid_attribute, weights=[0.3,0.4,0.3])
-            grid[point] = tile
-            counts[tile] += 1
+    for x in range(size):
+        for y in range(size):
+            grid[(x, y)] = random.choice(terrain_types)
 
-    return grid,counts
+    return grid
+
+print(generate_grid(10))
+
 
