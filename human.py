c720cd3729a8dfa40b08865e7db34540947f4cee
Behrad <soltanibehrad@gmail.com>
2026-02-25 19:10:20 +0330

    Started making the move method for the human class
    assigned x & y to the position of the human
    For now, diagonal movements are not added due
    to complexity but will added shortly in the
    not so distant future
    Also the movement will be chosen randomly and after
    vision is added to the human capabilities,
    they will be able to move smarter than just random!

--- human.py
+++ human.py
@@ -9,4 +9,12 @@ class Human:
         self.position = random_cell(grid)
         self.energy = None
 
+    def move(self):
+        x = self.position[0]
+        y = self.position[1]
+        movements = {"Up": (x, y + 1), "Down": (x, y - 1),
+                     "Left": (x - 1, y), "Right": (x + 1, y)}
+
+
+
 
