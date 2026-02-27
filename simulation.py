06e370302a8d1152a0f33b8ac2e4286c5074e25c
Behrad <soltanibehrad@gmail.com>
2026-02-27 19:07:59 +0330

    Added the tick mechanism
    The simulation has officially come alive!
    Now all humans can move in the desired tick
    for test, the movement of the first human is shown

--- simulation.py
+++ simulation.py
@@ -7,4 +7,9 @@ tick = 0
 for agent in range(100):
     agent = human.Human(world)
     list_of_humans.append(agent)
-
+for ticks in range(1000):
+    h0_position = list_of_humans[0].position
+    for agents in list_of_humans:
+        agents.move()
+    print(f"{h0_position} ---> {list_of_humans[0].position}")
+    tick+= 1
